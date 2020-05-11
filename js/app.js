4f5e281727a938b7ed53ce0e2d53b99783b9bc2b
aulia09 <65188966+aulia09@users.noreply.github.com>
2020-05-12 01:51:48 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,6 @@
-const navSlide = () => {
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
 
-    const menu = document.querySelector('.menu');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li ');
-    // Toggle Button
-    menu.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        // Animasi menu slider
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwads ${index / 7 + 1.5}s`;
-            }
-        });
-    });
-
-}
-
-navSlide();
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('slide');
+});
